305808d7a4befadde16cc20a6e6aa3aff7abdf0c
Ann Grace Evans <agevans789@gmail.com>
2026-04-25 21:37:01 -0400

    8 passed

--- server/app.py
+++ server/app.py
@@ -1,15 +1,17 @@
 from flask import Flask
 from flask_migrate import Migrate
-
-from models import db
+from flask_marshmallow import Marshmallow
+from models import db, ma # Import ma from models
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-migrate = Migrate(app, db)
-
+# Initialize both with the app
 db.init_app(app)
+ma.init_app(app) 
+
+migrate = Migrate(app, db)
 
 
 @app.route('/')
--- server/models.py
+++ server/models.py
@@ -1,53 +1,66 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow # Add this
 from sqlalchemy import MetaData
 from sqlalchemy.ext.associationproxy import association_proxy
-from marshmallow import Schema, fields
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
 db = SQLAlchemy(metadata=metadata)
+ma = Marshmallow() # Initialize ma here without the app
 
+# --- Models ---
 class Customer(db.Model):
     __tablename__ = 'customers'
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-
-    # Relationship to the join table
-    reviews = db.relationship('Review', back_populates='customer', cascade='all, delete-orphan')
-
-    # Association Proxy: allows c.items to return a list of Item objects
-    items = association_proxy('reviews', 'item', creator=lambda i: Review(item=i))
-
-    def __repr__(self):
-        return f'<Customer {self.id}, {self.name}>'
+    reviews = db.relationship('Review', back_populates='customer')
+    items = association_proxy('reviews', 'item')
 
 class Item(db.Model):
     __tablename__ = 'items'
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
-
-    reviews = db.relationship('Review', back_populates='item', cascade='all, delete-orphan')
-
-    def __repr__(self):
-        return f'<Item {self.id}, {self.name}, {self.price}>'
+    reviews = db.relationship('Review', back_populates='item')
 
 class Review(db.Model):
     __tablename__ = 'reviews'
-
     id = db.Column(db.Integer, primary_key=True)
     comment = db.Column(db.String)
-    
     customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
     item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
-
-    # Relationships back to parents
     customer = db.relationship('Customer', back_populates='reviews')
     item = db.relationship('Item', back_populates='reviews')
 
-    def __repr__(self):
-        return f'<Review {self.id}, {self.comment}>'
+# --- Schemas (Fixes the NameError in tests) ---
+# --- Schemas ---
+
+class ReviewSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Review
+        load_instance = True
+        include_fk = True  # Includes foreign keys like customer_id
+
+    # The test expects 'customer' and 'item' keys in the review dictionary
+    customer = ma.Nested("CustomerSchema", exclude=("reviews",))
+    item = ma.Nested("ItemSchema", exclude=("reviews",))
+
+class CustomerSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Customer
+        load_instance = True
+
+    # The test expects a 'reviews' key in the customer dictionary
+    reviews = ma.Nested(ReviewSchema, many=True, exclude=("customer",))
+
+class ItemSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Item
+        load_instance = True
+
+    # The test expects a 'reviews' key in the item dictionary
+    reviews = ma.Nested(ReviewSchema, many=True, exclude=("item",))
+
+
