b89624430462ff0f1d74a34977b6c7e4c7d0d036
Ann Grace Evans <agevans789@gmail.com>
2026-04-25 21:28:20 -0400

    5 passed

--- server/models.py
+++ server/models.py
@@ -3,13 +3,11 @@ from sqlalchemy import MetaData
 from sqlalchemy.ext.associationproxy import association_proxy
 from marshmallow import Schema, fields
 
-
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
 db = SQLAlchemy(metadata=metadata)
-
 
 class Customer(db.Model):
     __tablename__ = 'customers'
@@ -17,9 +15,14 @@ class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    # Relationship to the join table
+    reviews = db.relationship('Review', back_populates='customer', cascade='all, delete-orphan')
+
+    # Association Proxy: allows c.items to return a list of Item objects
+    items = association_proxy('reviews', 'item', creator=lambda i: Review(item=i))
+
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
-
 
 class Item(db.Model):
     __tablename__ = 'items'
@@ -28,5 +31,23 @@ class Item(db.Model):
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    reviews = db.relationship('Review', back_populates='item', cascade='all, delete-orphan')
+
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+    
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    # Relationships back to parents
+    customer = db.relationship('Customer', back_populates='reviews')
+    item = db.relationship('Item', back_populates='reviews')
+
+    def __repr__(self):
+        return f'<Review {self.id}, {self.comment}>'
